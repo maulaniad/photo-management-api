8c29671746c31fddf33ffd49a124e39a05299ea8
Adrian Maulani <maulani.ad@gmail.com>
2025-01-24 01:24:53 +0700

    feat(profile-model): change the upload destination

--- database/models/profile.py
+++ database/models/profile.py
@@ -3,11 +3,18 @@ from django.db.models import CharField, FileField
 from database.models.base import BaseModel
 
 
+def upload_avatar(instance: "Profile", filename: str):
+    folder_name = instance.oid
+    extension = filename.split(".")[-1]
+    new_filename = f"avatar.{extension}"
+    return f"avatars/{folder_name}/{new_filename}"
+
+
 class Profile(BaseModel):
     name = CharField(max_length=50, null=True, db_index=True)
     phone = CharField(max_length=15, null=True)
     address = CharField(max_length=255, null=True)
-    avatar = FileField(upload_to="avatar/", null=True)
+    avatar = FileField(upload_to=upload_avatar, null=True)
 
     class Meta(BaseModel.Meta):
         db_table = "auth_user_profile"
