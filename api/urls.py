1519fb4220ab5d3053872bb7938d04d883a5afcf
Adrian Maulani <maulani.ad@gmail.com>
2025-01-30 17:30:30 +0700

    refactor: make url for auth clearer

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("auth/", include("api.authentication.urls", namespace="authentication")),
+    path("authentication/", include("api.authentication.urls", namespace="authentication")),
     path("user/", include("api.user.urls", namespace="user")),
     path("profile/", include("api.profile.urls", namespace="profile")),
 ]
