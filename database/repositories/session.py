5db10eb02d2cbf6f2dc4614cbf522cc95f058c77
Adrian Maulani <maulani.ad@gmail.com>
2025-02-02 21:18:07 +0700

    chore: add comments for future me

--- database/repositories/session.py
+++ database/repositories/session.py
@@ -25,12 +25,21 @@ class SessionRepo:
         ).first()
 
     @staticmethod
-    def create_session(session_code: str, session_name: str | None = None, date_taken: datetime | None = None):
-        return Session.objects.create(
-            date_taken=date_taken,
-            session_code=session_code,
-            session_name=session_name
-        )
+    def create_session(session_code: str | None = None, session_name: str | None = None, date_taken: datetime | None = None):
+        # TODO: Refactor, seperate validation from repository
+        # This code requires refactoring, currently it intercepts all optional values
+        # so that Django ORM wouldn't think it gives null/None value
+        # and will fire the default function from the model instead
+        data_dict = {}
+
+        if session_code:
+            data_dict['session_code'] = session_code
+        if session_name:
+            data_dict['session_name'] = session_name
+        if date_taken:
+            data_dict['date_taken'] = date_taken
+
+        return Session.objects.create(**data_dict)
 
     @staticmethod
     def manager():
